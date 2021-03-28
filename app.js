0e202bf2bdde26f7cdcc3c10d6bec07d83843ad1
bradychen <bradychen2@gmail.com>
2021-03-28 17:54:45 +0800

    feat: register & homepage & detail routers

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const session = require('express-session')
 const methodOverride = require('method-override')
+const db = require('./models')
 const bcrypt = require('bcryptjs')
 
 const app = express()
 const PORT = 3000
+const Todo = db.Todo
+const User = db.User
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
@@ -26,15 +29,53 @@ app.get('/users/register', (req, res) => {
 })
 
 app.post('/users/register', (req, res) => {
-  console.log('register')
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ where: { email } })
+    .then(user => {
+      if (user) {
+        console.log('User already exists!')
+        return res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      }
+    }).catch(err => console.log(err))
+  return bcrypt.genSalt(10)
+    .then(salt => {
+      bcrypt.hash(password, salt)
+    })
+    .then(hash => {
+      User.create({
+        name,
+        email,
+        password: hash
+      })
+    })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
 })
 
 app.get('/users/logout', (req, res) => {
   console.log('logout')
 })
 
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(err => console.log(err))
+})
+
 app.get('/', (req, res) => {
-  res.send('hello world!')
+  return Todo.findAll({
+    // Transform to JS objects
+    raw: true,
+    nest: true
+  })
+    .then((todos) => res.render('index', { todos }))
+    .catch(err => res.status(422).json(err))
 })
 
 app.listen(PORT, () => {
